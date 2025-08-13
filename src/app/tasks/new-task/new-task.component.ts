8ffcd8ec4b7253cdea26d582e955ccce7aa8d6ae
steingartpeter <steingart.p2@gmail.com>
2025-08-13 11:50:26 +0200

    Shortened correct moethod for INJECTING the service

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -12,14 +12,14 @@ import { TasksService } from '../tasks.service';
 })
 export class NewTaskComponent {
   private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
-  private tasksService:TasksService;
 
-  constructor(){
-    this.tasksService = new TasksService();
+
+  constructor(private tService:TasksService){
+
   }
 
   onAddTask(title: string, description: string) {
-    this.tasksService.addTask({title:title,description:description})
+    this.tService.addTask({title:title,description:description})
     this.formEl()?.nativeElement.reset();
   }
 }
