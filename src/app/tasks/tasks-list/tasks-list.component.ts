71a3b585a7d95f15fe6ded1b93d8f4bff66915b5
steingartpeter <steingart.p2@gmail.com>
2025-08-13 13:55:42 +0200

    Fiishing up service function (filter, and status update)

--- src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
 import { TasksService } from '../tasks.service';
@@ -12,9 +12,29 @@ import { TasksService } from '../tasks.service';
 })
 export class TasksListComponent {
   private tService = inject(TasksService);
-  selectedFilter = signal<string>('all');
+  private selectedFilter = signal<string>('all');
   //tasks = [];
-  tasks = this.tService.allTasks;
+  tasks = computed(() => {
+    switch (this.selectedFilter()) {
+      // case 'all':
+      //   return this.tService.allTasks();
+      //   break;
+      case 'open':
+        return this.tService.allTasks().filter((tsk) => tsk.status === 'OPEN');
+        break;
+      case 'in-progress':
+        return this.tService
+          .allTasks()
+          .filter((tsk) => tsk.status === 'IN_PROGRESS');
+        break;
+      case 'done':
+        return this.tService.allTasks().filter((tsk) => tsk.status === 'DONE');
+        break;
+      default:
+        return this.tService.allTasks();
+        break;
+    }
+  });
 
   // constructor(private tService:TasksService){
 
--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, signal } from '@angular/core';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +7,7 @@ import { Task } from './task.model';
 export class TasksService {
   private tasks = signal<Task[]>([]);
   allTasks = this.tasks.asReadonly();
-  
+
   constructor() { 
     console.log('Task service constructed...');
   }
@@ -16,6 +16,18 @@ export class TasksService {
     const newTask:Task = {...taskData, id:Math.random().toString(),status:'OPEN'};
     this.tasks.update((oldTasks) => {
       return [...oldTasks, newTask];
+    })
+  }
+
+  updateTaskStatus(taskId:string, newStatus:TaskStatus){
+    this.tasks.update((oldTasks)=>{
+      return oldTasks.map((task)=>{
+        if(task.id ===taskId){
+          return {...task,status:newStatus};
+        }else{
+          return task;
+        }
+      })
     })
   }
 }
